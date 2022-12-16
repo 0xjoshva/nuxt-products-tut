e07b5fcb763ea6aef8569025f19df49b1405ab83
0xjoshva <joshuasteed123@gmail.com>
2022-12-16 15:25:19 +0200

    added error status page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>{{ error.statusCode }}</p>
+    <p>oops you visited the wrong page</p>
+    <p>{{ error.message }}</p>
+    <nuxt-link to="/">Go back home</nuxt-link>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"])
+</script>
+
+<style scoped></style>
